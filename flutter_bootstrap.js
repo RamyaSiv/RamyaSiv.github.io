538a8656d3f3b42f47e8d3ed111d0e2bf6cc9e23
Ramya <141208577+RamyaSiv@users.noreply.github.com>
2025-03-11 17:52:04 +0530

    http added

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"f73bfc4522dd0bc87bbcdb4bb3088082755c5e
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "2350015434"
+    serviceWorkerVersion: "2585675559"
   }
 });
